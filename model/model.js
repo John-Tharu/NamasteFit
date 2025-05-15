74005f4c6aadecc9dbdef2e8b895ee4edabd8b48
John Tharu <johntharu24@gmail.com>
2025-05-15 21:01:34 +0545

    Added JWT middleware

--- model/model.js
+++ model/model.js
@@ -1,27 +1,34 @@
-import { eq } from "drizzle-orm"
-import { db } from "../config/db.js"
-import { usersTable } from "../drizzle/schema.js"
-import argon2 from "argon2"
-import jwt from "jsonwebtoken"
+import { eq } from "drizzle-orm";
+import { db } from "../config/db.js";
+import { usersTable } from "../drizzle/schema.js";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
 
-export const checkEmail = async (email) =>{
-    return await db.select().from(usersTable).where(eq(usersTable.email,email));
-}
+export const checkEmail = async (email) => {
+  return await db.select().from(usersTable).where(eq(usersTable.email, email));
+};
 
-export const hashpass = async (password) =>{
-    return await argon2.hash(password);
-}
+export const hashpass = async (password) => {
+  return await argon2.hash(password);
+};
 
-export const saveData = async ({name,email,pass}) =>{
-    return await db.insert(usersTable).values({name,email,pass}).$returningId();
-}
+export const saveData = async ({ name, email, pass }) => {
+  return await db
+    .insert(usersTable)
+    .values({ name, email, pass })
+    .$returningId();
+};
 
-export const checkPass = async (hash,pass) =>{
-    return await argon2.verify(hash,pass);
-}
+export const checkPass = async (hash, pass) => {
+  return await argon2.verify(hash, pass);
+};
 
-export const generateToken = ({id,name,email}) =>{
-    return jwt.sign({id,name,email},process.env.JWT_SECRET,{
-        expiresIn: "30d"
-    })
-}
+export const generateToken = ({ id, name, email, role }) => {
+  return jwt.sign({ id, name, email, role }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+export const verifytoken = (token) => {
+  return jwt.verify(token, process.env.JWT_SECRET);
+};
--- router/router.js
+++ router/router.js
@@ -1,25 +1,38 @@
 import { Router } from "express";
-import { adminpage, cardpage, homepage, loginpage, planpage, programpage, registerpage, subscriptionpage, userpage } from "../controller/controller.js";
+import {
+  adminpage,
+  cardpage,
+  homepage,
+  loginpage,
+  logout,
+  planpage,
+  programpage,
+  registerpage,
+  subscriptionpage,
+  userpage,
+} from "../controller/controller.js";
 import { loginData, savedata } from "../controller/post.controller.js";
 
 const router = Router();
 
-router.route('/').get(homepage);
+router.route("/").get(homepage);
 
-router.route('/plan').get(planpage);
+router.route("/plan").get(planpage);
 
-router.route('/subscription').get(subscriptionpage);
+router.route("/subscription").get(subscriptionpage);
 
-router.route('/login').get(loginpage).post(loginData);
+router.route("/login").get(loginpage).post(loginData);
 
-router.route('/register').get(registerpage).post(savedata);
+router.route("/register").get(registerpage).post(savedata);
 
-router.route('/admin').get(adminpage);
+router.route("/admin").get(adminpage);
 
-router.route('/user').get(userpage);
+router.route("/user").get(userpage);
 
-router.route('/program').get(programpage);
+router.route("/program").get(programpage);
 
-router.route('/card').get(cardpage);
+router.route("/card").get(cardpage);
+
+router.route("/logout").get(logout);
 
 export const routerdata = router;
